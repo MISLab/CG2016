f9551e25186689f77f241cbfd5910a8d4ca396d1
Jalu <ck97756@yahoo.com.tw>
2016-02-29 01:38:14 +0800

    Add Google Analytics and Upgrade index

--- js/index.js
+++ js/index.js
@@ -1,45 +1,86 @@
-var weekNum=1;
-var courseTitle="Syllabus & Overview of Computer Graphics";
+var weekNum=2;
+var courseTitle="Computer Animation Pipeline";
 var courseInfo=[
-	"Introduction of computer graphics",
-	"The relationship between computer graphics, computer vision and image processing",
-	"Applications of Computer Graphics",
-	"Introduction of VFX"
+	"Introduction to computer animation pipeline",
+	"Modeling, texture, rigging, animation, shading, lighting, VFX (particles)",
+	"The idea of art-directable",
+	"Current challenges in computer animation production"
 ];
 var announcement=[
 	"Homework 1",
 	"Deadline: 2016/2/29 22:00"
 ];
+var title=[
+	{mainTitle:"Computer Graphic ", subTopic:"2016", img:"./art/title.png"},
+]
 
+var titleCount=title.length;
+var currentTitle=0;
+var changeTitle=true;
 var TitleElement=React.createClass({
 	render:function(){
 		return React.createElement(
-			"div",
-			null,
+			"a",
+			{className:"titleBody", id:"title"+this.props.count, href:this.props.href, style:{"background-image":"url("+this.props.img+")"}},
 			React.createElement(
 				"div",
-				{className:"titleBody"},
+				{className:"titleName"},
 				React.createElement(
 					"div",
-					{id:"titleName"},
-					"Computer Graphic ",
-					React.createElement(
-						"span",
-						{id:"year"},
-						"2016"
-					)
+					{className:"mainTitle"},
+					this.props.mainTitle
+				),
+				React.createElement(
+					"div",
+					{className:"subTopic"},
+					this.props.subTopic
 				)
+				
 			)
+		);
+	}
+});
+
+var TitleNavEle=React.createClass({
+	clickEvent:function(){
+		changeTitle=false;
+		changeTitlePage(this.props.num);
+	},
+	render:function(){
+		var className="titleNavEle";
+		if(this.props.num==currentTitle){
+			className="titleNavEle currentTitle";
+		}
+		return React.createElement(
+			"div",
+			{className:className, id:"titlePage"+this.props.num, onClick:this.clickEvent}
 		);
 	}
 });
 
 var Title=React.createClass({
 	render:function(){
+		var titleEle=[];
+		for(var a in title){
+			titleEle.push(React.createElement(TitleElement, {mainTitle:title[a].mainTitle, subTopic:title[a].subTopic, img:title[a].img, href:title[a].href, count:a}));
+		}
+		var titleNavEle=[];
+		for(var a in title){
+			titleNavEle.push(React.createElement(TitleNavEle, {num:a}));
+		}
 		return React.createElement(
 			"div",
 			{id:"title"},
-			React.createElement(TitleElement, null)
+			React.createElement(
+				"div",
+				{id:"innerTitle", style:{width:1024*titleCount+"px"}},
+				titleEle
+			),
+			React.createElement(
+				"div",
+				{id:"titleNav", style:{width:60*titleCount+"px"}},
+				titleNavEle
+			)
 		);
 	}
 });
@@ -121,5 +162,29 @@ var Main = React.createClass({
 		)
 	}
 });
+function changeTitlePage(num){
+	num=parseInt(num);
+	if(num>=titleCount){
+		return false;
+	}
+	document.getElementById("titlePage"+currentTitle).className="titleNavEle";
+	document.getElementById("titlePage"+num).className="titleNavEle currentTitle";
+	document.getElementById("innerTitle").style.left=-1024*num+"px"
+	currentTitle=num;
+}
+
+function autoChangeTitle(){
+	var nextTitle=currentTitle+1;
+	if(nextTitle>=titleCount){
+		nextTitle=0;
+	}
+	if(changeTitle==true){
+		changeTitlePage(nextTitle);
+	}else{
+		changeTitle=true;
+	}
+}
+
+setInterval(autoChangeTitle, 7000);
 currentPage.index="currentPage";
 ReactDOM.render(React.createElement(Main, null), document.getElementById("main"));
