5475b7b86b05a5098bd2c3f83886ad1e606314ff
黃均暉 <ck970756@gmail.com>
2016-04-16 15:48:23 +0800

    Weekly update

--- js/lecture.js
+++ js/lecture.js
@@ -4,7 +4,8 @@ var lectures=[
 	{title:"Week 3: Data Representation", img:"./art/week3.png", slideshow:"./file/20160308 Data Representation.pdf", video:"https://youtu.be/QxZXDUKvRUI"},
 	{title:"Week 4: Coordinates and Transformations", img:"./art/week4.png", slideshow:"./file/20160315 Coordinates and Transformations.pdf", video:"https://youtu.be/qxw18pHT9oU"},
 	{title:"Week 5: Talk by Phd. Rong-Hao Liang", img:"http://www.cmlab.csie.ntu.edu.tw/~howieliang/images/portrait2.png", slideshow:"./file/Talk by Phd. Rong-Hao Liang.pdf"},
-	{title:"Week 6: Shading", img:"./art/week6.png", slideshow:"./file/20160329 Basic Shading.pdf", video:"https://youtu.be/SRModLX8cOU"}
+	{title:"Week 6: Shading", img:"./art/week6.png", slideshow:"./file/20160329 Basic Shading.pdf", video:"https://youtu.be/SRModLX8cOU"},
+	{title:"Week 7: Mapping", img:"./art/week7.jpg", slideshow:"./file/20160412 Mapping.pdf", video:"https://youtu.be/GMwqkfSiggo"}
 ]
 
 var Lecture=React.createClass({
